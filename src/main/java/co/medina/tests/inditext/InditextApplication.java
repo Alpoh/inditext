d00b1289db2f3442f366e3e498657590ea10d977
cmedina <cmedina@ces.vocento.com>
2023-03-12 12:31:27 +0100

    main initilzr code

---
+++ src/main/java/co/medina/tests/inditext/InditextApplication.java
@@ -0,0 +1,13 @@
+package co.medina.tests.inditext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InditextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/medina/tests/inditext/InditextApplicationTests.java
@@ -0,0 +1,13 @@
+package co.medina.tests.inditext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InditextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
