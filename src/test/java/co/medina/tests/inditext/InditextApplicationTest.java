cc6d4be03eb3673cd21ada52f56e1dab36e641bd
cmedina <cmedina@ces.vocento.com>
2023-03-15 06:11:28 +0100

    feature/test add resources and ignore flyway

---
+++ src/test/java/co/medina/tests/inditext/InditextApplicationTest.java
@@ -0,0 +1,16 @@
+package co.medina.tests.inditext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InditextApplicationTest {
+
+  @Test
+  void main() {
+    Assertions.assertTrue(Boolean.TRUE);
+  }
+}
